9aebc146342fd3608a93ed9bdd4404f2f9295605
Alex Bal <alexenri12@gmail.com>
2022-10-14 08:06:39 -0300

    add variables

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,6 +2,35 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Subiendo mi primer proyecto Java a GitHub");
+        // Tipos de datos
+
+        //enteros
+        byte variable1=2;
+        short variable2 = 10;
+        int variable3=30;
+        long variable4= 123456789;
+
+        //decimales
+        float variable5= 1.1f;
+        double variable6 =1.2d;
+
+        //boleano
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //texto
+        char letra= 'a';
+        String texto= "Hola Java";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(verdadero);
+        System.out.println(falso);
+        System.out.println(letra);
+        System.out.println(texto);
     }
 }
